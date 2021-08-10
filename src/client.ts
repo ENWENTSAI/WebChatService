00b75e20af038ca1da17d4c13f29f575ebd21694
Alison TSAI <tsaiwenwen0304@gmail.com>
2021-08-11 00:49:02 +0800

    update

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,26 @@
 //import { Socket } from "socket.io";
-import { io } from "socket.io-client";
+//import { io } from "socket.io-client";
 class Client{
+   private socket:SocketIOClient.Socket;
    constructor(){
-    const socket = io("http://localhost:8080",{transports :  ['websocket', 'polling', 'flashsocket'] });
+      this.socket = io();
 
-     socket.emit("message")
+      this.socket.on('message', function (message: any) {
+         console.log(message)
+         document.body.innerHTML = message
+     })
+
+
+
+   //const socket = io("http://localhost:8080",{transports :  ['websocket', 'polling', 'flashsocket'] });
+   //    socket.on("connect", function(){
+   //       console.log("connect");
+   //    })
+   //    socket.on("disconnect", function (message: any) {
+   //       console.log("disconnect " + message);
+   //   })
+
+   //    socket.emit("message")
    }
 }
 
