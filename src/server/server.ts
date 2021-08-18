bedd291fb86c4b5d9d5adc35f908c8f62854f861
Alison TSAI <tsaiwenwen0304@gmail.com>
2021-08-18 15:27:15 +0800

    update

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,7 @@ import * as http from "http"
 import * as socketIO from "socket.io"
 
 const port : number = 4000;
+
 class Server{
 
   private server:http.Server;
@@ -11,55 +12,55 @@ class Server{
 
   constructor(port: number){
     this.port=port;
+
     const app =express();
     app.use(express.static('public'));
 
     this.server=new http.Server(app);
     this.io = new socketIO.Server(this.server);
 
+    
     this.io.on('connection', (socket: socketIO.Socket) => {
       
       console.log('a user connected : ' + socket.id)
-
-      // listening the socket for getting the room & username from client side
+      
+      // get the room & username value from client side
       socket.on('joinRoom', function (room: string,username:string) {
         
         // use join function to join the specific room
-
         socket.join(room);
 
-        //broadcast the message who joined this specific room 
-        socket.broadcast.to(room).emit('message',username + " has joined the room - " + room);
+        //broadcast the message who joined this specific room.
+        socket.broadcast.to(room).emit('message'," Everybody, say hello to  " + username);
 
-        //sending chat message to who are in the same room. 
+
+        //sending messages to this specific room. 
         socket.on('chatMessage', function (chatMessage: ChatMessage) {
           console.log('chatMessage', chatMessage);
-          socket.broadcast.to(room).emit('chatMessage', chatMessage)
+          socket.broadcast.to(room).emit('chatMessage', chatMessage);
+        });
+      
+      });
+    
+    
+      //getting the room & username from client side for disconnect message
+      socket.on('LeftRoom', function (room: string,username:string) {
+        socket.join(room);
+
+        socket.on('disconnect', () => {
+          console.log(socket.id + ' has left');
+          socket.broadcast.to(room).emit('message',username + " has left ");
+        });
+
       });
 
-    });
-    socket.on('LeftRoom', function (room: string,username:string) {
-      socket.join(room);
-
-      socket.on('disconnect', () => {
-        console.log( socket.id + 'has left') ;
-        socket.broadcast.to(room).emit('message',username + " has left ");
-    });
-
-  });
-      
-      // socket.on('disconnect', () => {
-      //     console.log( socket.id + 'has left') ;
-      //     socket.broadcast.to(room).emit('message',username + " has left ");
-      // });
-
-  })
+     });
 
   }
   public Start() {
     this.server.listen(this.port)
     console.log(`Server listening on port ${this.port}.`)
-}
+  }
 
 }
 
